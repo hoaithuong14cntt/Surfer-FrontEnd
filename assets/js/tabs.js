58d78bd363738d34e49f11b29a664e180f4e5661
Hoai Thuong <thuong.hth@neo-lab.vn>
2017-12-07 17:06:24 +0700

    * fix js

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   $('.introduce_tabs-item').click(function(){
-    // var id = $(this).attr('id');
-    // console.log(id);
     $('.introduce_tabs-item').removeClass('active');
     $(this).addClass('active');
-    $("#description").click(function(){
+  });
+});
+ $(document).ready(function(){
+  $("#description").click(function(){
       $(".description").show();
       $(".feature").hide();
       $(".dimension").hide();
@@ -20,5 +21,3 @@ $(document).ready(function(){
       $(".dimension").show();
     });
   });
-});
- 
